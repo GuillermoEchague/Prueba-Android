6eeeefbe421e0259abebf4983e027ded7f2c0c8c
Guillermo <guillermo.echaguea@gmail.com>
2020-03-20 03:10:02 -0300

    Fragmento Favorito con boton favorito

--- app/src/main/java/com/example/pruebaandroid/MainActivity.java
+++ app/src/main/java/com/example/pruebaandroid/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.pruebaandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.RecyclerView;
@@ -20,6 +21,7 @@ import com.example.pruebaandroid.model.BreedListResponse;
 import com.example.pruebaandroid.ui.FragmentDetails;
 import com.example.pruebaandroid.ui.FragmentFavourite;
 import com.example.pruebaandroid.ui.FragmentListBreeds;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.List;
 
@@ -29,14 +31,11 @@ import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
-  //  private RecyclerView mRecyclerView;
-  //  FragmentDetails fragmentDetails;
-  //  FragmentFavourite fragmentFavourite;
-  //  FragmentListBreeds fragmentListBreeds;
+    FragmentFavourite fragmentFavourite;
 
     private String perro1;
-
-    Button btnFavorito;
+    private FirebaseFirestore db;
+    private Button btnFavorito;
     TextView textView;
     FrameLayout frameLayout;
 
@@ -45,15 +44,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-    //    mRecyclerView = findViewById(R.id.recyclerview);
-    //    fragmentListBreeds = new FragmentListBreeds();
-    //    fragmentFavourite = new FragmentFavourite();
-    //    fragmentDetails = new FragmentDetails();
+        fragmentFavourite = new FragmentFavourite();
 
-    //    getSupportFragmentManager().beginTransaction().add(R.id.fragments,fragmentListBreeds).commit();
-
-                // API
-
+        // API
         ApiDog service = RetrofitClient.getRetrofitInstance().create(ApiDog.class);
         Call<BreedListResponse> call = service.getBreedList();
 
@@ -61,15 +54,12 @@ public class MainActivity extends AppCompatActivity {
         call.enqueue(new Callback<BreedListResponse>() {
             @Override
             public void onResponse(Call<BreedListResponse> call, Response<BreedListResponse> response) {
-
                 initializeViews();
-
                 if (response.body() != null) {
                     List<String> perritos = response.body().getBreedList();
                     Log.e("PERRITOS", String.valueOf(perritos));
                     inFragment(perritos);
                 }
-
             }
             @Override
             public void onFailure(Call<BreedListResponse> call, Throwable t) {
@@ -79,72 +69,42 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        // Desarrollo Conexión BD Firestore
+
+        // db = FirebaseFirestore.getInstance();
+
         btnFavorito = findViewById(R.id.btnFavorito);
         btnFavorito.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(getApplicationContext(),"Fragment Favorito",Toast.LENGTH_SHORT).show();
-                FragmentManager manager = getSupportFragmentManager();
-                FragmentFavourite favourite = new FragmentFavourite();
-                favourite.show(manager);
+                getSupportFragmentManager().beginTransaction().
+                        add(R.id.fragments,fragmentFavourite).
+                        addToBackStack(null).
+                        commit();
             }
         });
     }
 
+     public void onClick(View view) {
+                getSupportFragmentManager().beginTransaction().
+                add(R.id.fragments, fragmentFavourite).addToBackStack(null)
+                   .commit();
+     }
 
-    /*
-    public void onClick(View view) {
-        FragmentTransaction transaction =  getSupportFragmentManager().beginTransaction();
-        switch (view.getId()){
-            case R.id.btnFavorito:
-                transaction.replace(R.id.fragments,fragmentFavourite);
-            break;
-            case R.id.btnLista:
-                transaction.replace(R.id.fragments,fragmentListBreeds);
-                break;
-            case R.id.btnDetalles:
-                transaction.replace(R.id.fragments,fragmentDetails);
-                break;
-            default:
-                transaction.replace(R.id.fragments,fragmentFavourite);
-                break;
-        }
-           transaction.commit();
-    }
-*/
     private void inFragment(List<String> list){
       FragmentListBreeds fragmentListBreeds = FragmentListBreeds.newInstance(list);
       getSupportFragmentManager()
               .beginTransaction()
               .add(R.id.fragments, fragmentListBreeds, fragmentListBreeds.getClass().getSimpleName())
       .commit();
+
     }
 
-/*
-    private void whoLetTheDogsOut(){
-        ApiDog service = RetrofitClient.getRetrofitInstance().create(ApiDog.class);
-        Call<BreedImageListResponse> callImages = service.getBreedImageList(perro1);
-        callImages.enqueue(new Callback<BreedImageListResponse>() {
-            @Override
-            public void onResponse(Call<BreedImageListResponse> call, Response<BreedImageListResponse> response) {
-                List<String> imagesURL = response.body().getImageURL();
-                Log.e("IMAGESDOGS", String.valueOf(imagesURL));
-            }
-            @Override
-            public void onFailure(Call<BreedImageListResponse> call, Throwable t) {
-                Log.e("FALLO", String.valueOf(t));
-            }
-        });
-    }
-
-*/
     private void initializeViews(){
         textView = findViewById(R.id.textListDog);
         frameLayout = findViewById(R.id.fragments);
     }
 
 
-    public void onClick(View view) {
-
-    }
 }
--- app/src/main/java/com/example/pruebaandroid/ui/FragmentFavourite.java
+++ app/src/main/java/com/example/pruebaandroid/ui/FragmentFavourite.java
@@ -8,6 +8,7 @@ import androidx.fragment.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.pruebaandroid.R;
 
@@ -26,6 +27,7 @@ public class FragmentFavourite extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
 
     public FragmentFavourite() {
         // Required empty public constructor
@@ -62,9 +64,12 @@ public class FragmentFavourite extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+
+
+
         return inflater.inflate(R.layout.fragment_favourite, container, false);
     }
 
-    public void show(FragmentManager manager) {
-    }
+
+
 }
